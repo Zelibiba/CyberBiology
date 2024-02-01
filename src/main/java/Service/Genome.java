118287cfa0048b83e2b7086391bf9afe6cc5d69f
Дмитрий Дугин <dugin-di@yandex.ru>
2024-02-01 19:53:14 +0300

    Создан класс Genome.

---
+++ src/main/java/Service/Genome.java
@@ -0,0 +1,68 @@
+package Service;
+
+import java.util.Random;
+
+/**
+ * Класс генома бота.
+ */
+public class Genome {
+
+    /**
+     * Размер генома.
+     */
+    private static final int SIZE = 64;
+    /**
+     * Максимальное значение генома.
+     */
+    private static final int MAX_GENE = 68;
+    /**
+     * Шанс мутирования при копировании генома.
+     */
+    private static final int MUTATION_CHANCE = 50;
+
+    public Genome() {
+        massive = new byte[SIZE];
+        for (int i = 0; i < SIZE; i++)
+            massive[i] = 0;
+    }
+
+    /**
+     * Копирует геном с шансом мутации.
+     * @param genome Источник генома.
+     * @param mutation Включает шанс мутации.
+     */
+    public Genome(Genome genome, boolean mutation) {
+
+        massive = genome.massive.clone();
+        if (mutation) {
+            Random rand = new Random();
+            if (rand.nextInt(100) < MUTATION_CHANCE)
+                massive[rand.nextInt(SIZE)] = (byte) rand.nextInt(MAX_GENE + 1);
+        }
+    }
+
+    private final byte[] massive;
+    private int pointer = 0;
+
+    /**
+     * Добавляет к указателю на ген число.
+     */
+    public void addToPointer(int value) {
+        pointer = (pointer + value) % SIZE;
+    }
+
+    /**
+     * Возвращает ген, на который указывает указатель.
+     */
+    public int getGene() {
+        return massive[pointer];
+    }
+
+    /**
+     * Возвращает ген, расположенный на расстоянии delta вперёд от текущего положения указателя.
+     * @param delta Расттояние от гена до указателя.
+     */
+    public int getGene(int delta) {
+        return massive[(pointer + delta) % SIZE];
+    }
+}
--- src/main/java/Service/Orientation.java
+++ src/main/java/Service/Orientation.java
@@ -6,53 +6,57 @@ import java.util.Random;
  * Перечисление ориентации бота.
  */
 public enum Orientation {
-    UP(0), UPRIGHT(1),RIGHT(2),DOWNRIGHT(3),
-    DOWN(4),DOWNLEFT(5),LEFT(6),UPLEFT(7);
+    UP(0), UPRIGHT(1), RIGHT(2), DOWNRIGHT(3),
+    DOWN(4), DOWNLEFT(5), LEFT(6), UPLEFT(7);
 
-    private static final Orientation []MASSIVE = Orientation.values();
-    private static final int SIZE= MASSIVE.length;
-    private static final int [][]DIRECTIONS=new int[][]{
-            new int[]{-1,0},
-            new int[]{-1,1},
-            new int[]{0,1},
-            new int[]{1,1},
-            new int[]{1,0},
-            new int[]{1,-1},
-            new int[]{0,-1},
-            new int[]{-1,-1},
+    private static final Orientation[] MASSIVE = Orientation.values();
+    private static final int SIZE = MASSIVE.length;
+    private static final int[][] DIRECTIONS = new int[][]{
+            new int[]{-1, 0},
+            new int[]{-1, 1},
+            new int[]{0, 1},
+            new int[]{1, 1},
+            new int[]{1, 0},
+            new int[]{1, -1},
+            new int[]{0, -1},
+            new int[]{-1, -1},
     };
 
-    Orientation(int value){
-        this.value=value;
+    Orientation(int value) {
+        this.value = value;
     }
+
     private int value;
 
     /**
      * Метод возвращает вектор, соответсвующий данному направлению.
      */
-    public int []getDirection(){
+    public int[] getDirection() {
         return DIRECTIONS[value];
     }
+
     /**
      * Метод поворачивает ориентацию по часовой стрелке.
+     *
      * @param angle Индекс угла поворота.
      * @return Поввёрнутая ориентация.
      */
-    public Orientation spin(int angle){
-        return MASSIVE[(value+angle)%SIZE];
+    public Orientation spin(int angle) {
+        return MASSIVE[(value + angle) % SIZE];
     }
 
     /**
      * Возвращает случайную ориентацию.
      */
-    public static Orientation randomize(){
+    public static Orientation randomize() {
         Random rand = new Random();
         return MASSIVE[rand.nextInt(SIZE)];
     }
+
     /**
      * Возвращает оринтацию, соответствующую углу.
      */
-    public static Orientation getOrientation(int angle){
-        return MASSIVE[angle%SIZE];
+    public static Orientation getOrientation(int angle) {
+        return MASSIVE[angle % SIZE];
     }
 }
